f15a8c287bc863906cde6630ff63b3e2c22e78c5
Marek Loníček <mark.lonicek@seznam.cz>
2023-04-25 06:51:31 +0200

    first commit

---
+++ PizzeriaRazor/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPizzeria.Models;
+
+
+namespace RazorPizzeria.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<PizzaOrder>? PizzaOrders { get; set; }
+       
+    }
+}
---
+++ PizzeriaRazor/Models/PizzaOrder.cs
@@ -0,0 +1,11 @@
+﻿namespace RazorPizzeria.Models
+{
+    public class PizzaOrder
+    {
+        public int Id { get; set; }
+        public string? PizzaName { get; set; }
+        public float? PizzaPrice { get; set; }
+        public DateTime PizzaDate { get; set; } = DateTime.Now;
+		public bool PremiumPresentPackage { get; set; }
+	}
+}
